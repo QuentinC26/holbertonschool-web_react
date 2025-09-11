36993f59546525962fabb0e7c58622eac1f568c0
QuentinC26 <q.zuzlewski@gmail.com>
2025-09-12 01:07:11 +0200

    i add the interface, the two variables and the list

---
+++ TypeScript/task_0/js/main.ts
@@ -0,0 +1,23 @@
+interface Student {
+  firstName: string,
+  lastName: string,
+  age: number,
+  location: string
+}
+
+const student1: Student = {
+  firstName: "David",
+  lastName: "test",
+  age: 18,
+  location: "Chicago"
+}
+
+const student2: Student = {
+  firstName: "Patrick",
+  lastName: "test",
+  age: 18,
+  location: "Chicago"
+}
+
+const studentsList = [student1, student2];
+
