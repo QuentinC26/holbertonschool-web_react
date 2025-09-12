210242133dc4ea909c87f5ae5cc6593a70cda9fe
QuentinC26 <q.zuzlewski@gmail.com>
2025-09-12 10:00:25 +0200

    i add the array and the table with  for loop

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -21,3 +21,17 @@ const student2: Student = {
 
 const studentsList = [student1, student2];
 
+const theArray = [ [student1.firstName, student1.location], 
+  [student2.firstName, student2.location]]
+
+let table = document.createElement('table');
+for (let row of studentsList) {
+  table.insertRow();
+  for (let cell in row) {
+    let newCell = table.rows[table.rows.length - 1].insertCell();
+    newCell.textContent = cell;
+  }
+}
+document.body.appendChild(table);
+
+console.log(theArray)
